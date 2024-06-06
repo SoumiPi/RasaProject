d35ae074ccabade56150fe31f6513c846ae2e3f7
SoumiPi <147778229+SoumiPi@users.noreply.github.com>
2024-06-06 10:18:16 +0000

    Organisation des histoire d'interventions dans stories.yml

--- actions/actions.py
+++ actions/actions.py
@@ -50,4 +50,57 @@ class ConfirmOrderAction(Action):
         return []
 
 
-#Creation des acteurs
+#Extractin des periode d'interventions
+
+class ConfirmOrderAction(Action):
+    def name(self) -> Text:
+        return "action_interventions_periode"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        periode_entity = next(tracker.get_latest_entity_values('periode'), None)
+        if periode_entity:
+            if periode_entity == "aujourd'hui":
+                dispatcher.utter_message(text=f"Vous intervenez {periode_entity} aux machines y et y et votre interventions consite à .......")
+
+            if periode_entity == "mois":
+                dispatcher.utter_message(text=f"Vous devez intervenir ce {periode_entity} niveau des machines y et y et votre interventions consite à .......")
+
+            if periode_entity == "semaine":
+                dispatcher.utter_message(text=f"Vous intervenez cette {periode_entity} aux machines y et y et votre interventions consite à .......")
+
+            if periode_entity == "demain":
+                dispatcher.utter_message(text=f" {periode_entity}, vous intervenez aux machines y et y et votre interventions consite à .......")
+
+        else:
+            dispatcher.utter_message(text="Désolé, je n'arrive pas à extraire la période d'intervention.")
+
+        return []
+
+
+
+class ConfirmOrderAction(Action):
+    def name(self) -> Text:
+        return "action_interventions_priorite"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        priority_entity = next(tracker.get_latest_entity_values('priorite'), None)
+        if priority_entity:
+            if priority_entity == "urgemment":
+                dispatcher.utter_message(text=f"Vous intervenez {priority_entity} aux machines y et votre interventions consite à .......")
+
+            if priority_entity == "urgentes":
+                dispatcher.utter_message(text=f"Les interventions {priority_entity} sont celles  au niveau des machines y")
+
+            if priority_entity == "très urgemment" or "haute priorité" or "les plus urgentes" or "proritaire":
+                dispatcher.utter_message(text=f"Vos inteventions les plus urgentes sont aux machines XXX  .......")
+
+        else:
+            dispatcher.utter_message(text="Désolé, je n'arrive pas extraire les interventions voulues.")
+
+        return []
